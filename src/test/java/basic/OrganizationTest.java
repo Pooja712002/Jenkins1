b64af8abc39eb68c1b3601b5ad84719936661d7c
Pooja712002 <89062419+Pooja712002@users.noreply.github.com>
2024-03-16 13:15:28 +0530

    update project

---
+++ src/test/java/basic/OrganizationTest.java
@@ -0,0 +1,15 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	@Test
+	public void CreateorganizationnsTest() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://localhost:8888");
+		
+	}
+
+}
